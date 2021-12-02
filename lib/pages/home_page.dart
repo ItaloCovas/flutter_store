34fdd2b6845ec99f149868705c24cf821560ca8c
Ana Clara Fagundes <79975018+fagundesana017@users.noreply.github.com>
2021-12-02 20:10:58 -0300

    homepage e font

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,131 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_plus/flutter_plus.dart';
 import 'package:flutter_store/theme/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-  
-      resizeToAvoidBottomInset: false,
+      backgroundColor: primaryBlack,
       appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
         backgroundColor: primaryBlack,
+      ),
+      body: ContainerPlus(
+        child: TextPlus('Hey! Discover our good items. 👋',
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'RobotoMono',
+            padding: EdgeInsets.fromLTRB(20, 35, 20, 5)),
+      ),
+      bottomNavigationBar: BottomNavigationBar(),
+    );
+  }
+}
+
+class BottomNavigationBar extends StatefulWidget {
+  const BottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigationBar> createState() => _BottomNavigationBarState();
+}
+
+class _BottomNavigationBarState extends State<BottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ContainerPlus(
+      color: primaryBlack,
+      child: BottomAppBar(
+        elevation: 0,
+        color: primaryBlack,
+        child: SizedBox(
+          height: 65.0,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.local_grocery_store,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
+class IconButtomBar extends StatelessWidget {
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+
+  const IconButtomBar(
+      {required this.icon,
+      required this.selected,
+      required this.onPressed,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            icon,
+            size: 30,
+            color: selected ? Colors.white : Color(0xff646466),
+          ),
+        ),
+      ],
+    );
+  }
+}
