a2f39eba7c4649ec6b6e2b85dadba3d710c8fafc
Ítalo Covas <italocovas@gmail.com>
2021-12-02 15:54:27 -0300

    Atualizações na página de Login

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 
 const primaryBlack = Color(0xff161618);
-const secondaryBlack = Color(0xff171719);
+const secondaryBlack = Color(0xff232227);
 const buttonColor = Color(0xff4C75E2);
