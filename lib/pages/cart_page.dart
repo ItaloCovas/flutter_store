9c11b2c7a73d406a3c9c65f692425a572e2fa44b
Ana Clara Fagundes <79975018+fagundesana017@users.noreply.github.com>
2021-12-15 15:32:43 -0300

    api cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -19,34 +19,165 @@ class _CartPageState extends State<CartPage> {
           children: [
             ContainerPlus(
               width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.only(top: 40, right: 5, left: 15),
+              padding: EdgeInsets.only(top: 50, right: 10, left: 15),
               height: 300,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.arrow_back_ios,
-                        size: 20,
-                        color: Colors.white,
-                      )
-                    ],
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.arrow_back_ios,
+                          size: 20,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
                   ),
                   Padding(
                     padding: EdgeInsets.only(
                       top: 20,
                     ),
-                    child: TextPlus('Carrinho Padawans',
+                    child: TextPlus('Carrinho Padawans 🛒',
                         color: Colors.white,
                         fontSize: 30,
                         fontWeight: FontWeight.w400,
                         fontFamily: 'RobotoMono',
-                        padding: EdgeInsets.fromLTRB(5, 35, 17, 5)),
+                        padding: EdgeInsets.fromLTRB(0, 50, 10, 5)),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  color: secondaryBlack,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50))),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 18),
+                        child: TextPlus(
+                          'Subtotal',
+                          fontSize: 18,
+                          fontFamily: 'RobotoMono',
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey.shade300,
+                          letterSpacing: 1,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 360,
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20),
+                        child: ButtonPlus(
+                          width: 350,
+                          height: 50,
+                          radius: RadiusPlus.all(12),
+                          color: buttonColor,
+                          enabled: true,
+                          child: TextPlus(
+                            'Finalizar Compra',
+                            fontSize: 18,
+                            fontFamily: 'RobotoMono',
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
                   )
                 ],
               ),
-            )
+            ))
           ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(),
+    );
+  }
+}
+
+class BottomNavigationBar extends StatefulWidget {
+  const BottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigationBar> createState() => _BottomNavigationBarState();
+}
+
+class _BottomNavigationBarState extends State<BottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ContainerPlus(
+      color: primaryBlack,
+      child: BottomAppBar(
+        elevation: 0,
+        color: primaryBlack,
+        child: SizedBox(
+          height: 65.0,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.grey.shade800,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade800,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CartPage()),
+                    );
+                  },
+                  icon: Icon(
+                    Icons.local_grocery_store,
+                    color: Colors.grey.shade800,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.grey.shade800,
+                    size: 30,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
