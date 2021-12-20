58478256cd9334733318d5bb33f61dccf0e1b9b3
Ítalo Covas <italocovas@gmail.com>
2021-12-20 14:12:21 -0300

    Começando alterar visualmente a página do perfil

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_plus/flutter_plus.dart';
+import 'package:flutter_store/theme/colors.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({ Key? key }) : super(key: key);
@@ -12,7 +13,30 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextPlus('HELLO', color: Colors.white),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0, 80, 0, 0),
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: buttonColor,
+                              width: 2,
+                            ),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/profile.png'),
+                              fit: BoxFit.cover,
+                            ))),
+            ),
+            SizedBox(height: 10),
+            TextPlus('Nome Aqui', color: Colors.white, fontSize: 16),
+          ],
+        ),
+      ),
     );
   }
 }
